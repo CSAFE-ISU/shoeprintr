300f13e932e76c78fbf54764e943bcfb8be06049
Soyoung Park <sypark@iastate.edu>
2017-09-01 14:34:14 -0500

    Im just testing

--- R/functions.R
+++ R/functions.R
@@ -7,7 +7,7 @@
 "reference_example"
 
 #' @title Implementation of a smart sampling algorithm
-#' 
+#'
 #' @description Define a smart sampling function which will ensure uniform samples so that we can get better results even with a lower ncross value. This process will divide the input circle area into hexbins and assign probabilities to the bins which will be correlated with the number of points in the bin. This will result in sampling the points from high density areas while still ensuring a more uniform distribution.
 #' @name smart_sample
 #' @param pts_xy The set of points (for example, the input circle of a shoeprint)
@@ -18,10 +18,10 @@
 #' @return A data frame consisting of the sampled points from the original input circle
 #' @export
 #' @import hexbin
-#' @examples 
+#' @examples
 #' \dontrun{
 #' data(input_example)
-#' 
+#'
 #' smart_sample(input_example, sample_bins = NULL, sample_size = NULL) # All points from all bins
 #' smart_sample(input_example, sample_bins = 30 & sample_size = NULL) # All points from 30 bins
 #' smart_sample(input_example, sample_bins = NULL, sample_size = 30) # 30 points from all bins
@@ -31,15 +31,15 @@
 smart_sample <- function(pts_xy, sample_bins = NULL, sample_size = NULL, xbins = 20, seed = 1) {
     ## Generate a hexbin object
     pts_hxb <- hexbin(pts_xy,xbins=xbins,IDs=TRUE)
-    
+
     ## Assign cell ID to data points
     pts_xy$cell_id <- pts_hxb@cID
-    
+
     ## Get number of points in every bin and sample bins to choose points from
     hxb_stats <- data.frame(cell_id=pts_hxb@cell,count=pts_hxb@count)
     set.seed(seed)
     candidate_hxbs <- sample(hxb_stats$cell_id,ifelse(is.null(sample_bins),length(pts_hxb@cell),min(nrow(hxb_stats),sample_bins)),prob=hxb_stats$count/sum(hxb_stats$count))
-    
+
     ## Candidate points and final sample of points
     pts_xy <- pts_xy[pts_xy$cell_id %in% candidate_hxbs,]
     if(is.null(sample_size)) return(pts_xy[,-3])
@@ -85,23 +85,23 @@ get_clique_stats <- function(clique,c_in,circle_in,c_ref,circle_ref,la,lb,plot=T
     cq_idx <- as.data.frame(t(sapply(clique,function(z) c(i=ifelse(z %% lb==0,(z %/% lb),(z %/% lb)+1),j=ifelse(z %% lb==0,lb,z %% lb)))))
     c_in_cq <- c_in[cq_idx$i,]
     c_ref_cq <- c_ref[cq_idx$j,]
-    
+
     ## Calculate affine transformation parameters i.e Rotation and Translation
     affine_tfr_mat <- AffineTransformation(as.data.frame(cbind(c_in_cq,c_ref_cq)))
     calculateParameters(affine_tfr_mat)
-    
+
     ## Apply affine transformation to input circle and convert classes to spatial points
     circle_in_sps <- applyTransformation(affine_tfr_mat,SpatialPoints(circle_in))
     c_in_cq_sps <- applyTransformation(affine_tfr_mat,SpatialPoints(c_in_cq))
     circle_ref_sps <- SpatialPoints(circle_ref)
-    
+
     ## Prepare the match return statistics
     circle_in_ref_dist <- apply(spDists(circle_in_sps,circle_ref_sps),1,min)
     circle_in_dist_idx <- circle_in_sps@coords[circle_in_ref_dist<2,]
     circle_in_ref_dist_idx <- (circle_ref_sps[apply(spDists(circle_in_sps,circle_ref_sps),1,which.min)]@coords)[circle_in_ref_dist<2,]
     center_new <- apply(circle_in_ref_dist_idx,2,function(x) ((min(x) + max(x))/2))
     radius_new <- max(sqrt((circle_in_ref_dist_idx[,1]-center_new[1])^2+(circle_in_ref_dist_idx[,2]-center_new[2])^2))
-    
+
     ## Rotation Angle
     R= matrix(affine_tfr_mat@parameters[-c(3,6)],2,byrow=FALSE)
     out = svd(R)
@@ -113,20 +113,20 @@ get_clique_stats <- function(clique,c_in,circle_in,c_ref,circle_ref,la,lb,plot=T
     theta = atan(out$u[2,1]/out$u[1,1])*180/pi
     phi = atan(out$v[1,2]/out$v[1,1])*180/pi
     rot_angle = min(abs(theta+phi),180-abs(theta+phi))
-    
-    
+
+
     if(plot)
     {
         ## 2*2 Plot layout
         par(mfrow=c(2,2))
-        
-        ## Plot distance 
+
+        ## Plot distance
         plot(as.matrix(dist(c_in_cq)),as.matrix(dist(c_ref_cq)),xlab="",ylab="",main="Corresponding Distances Plot")
-        
+
         ## Plot matching points
         plot(c_ref_cq,col="red",pch=19,main="Matching Points")
         points(c_in_cq_sps,col="blue")
-        
+
         ## Plot all points
         plot(	circle_ref,col="red",
               xlim=c(min(circle_ref$X,circle_in_sps@coords[,1]),max(circle_ref$X,circle_in_sps@coords[,1])),
@@ -134,7 +134,7 @@ get_clique_stats <- function(clique,c_in,circle_in,c_ref,circle_ref,la,lb,plot=T
               pch=19,main="All Points"
         )
         points(circle_in_sps,col="blue",pch=19)
-        
+
         ## Plot Close points
         plot(	circle_in_ref_dist_idx,col="red",pch=19,
               xlim=c(min(circle_in_dist_idx[,1],circle_in_ref_dist_idx[,1]),max(circle_in_dist_idx[,1],circle_in_ref_dist_idx[,1])),
@@ -166,7 +166,7 @@ get_os <- function() {
 }
 
 #' Function to perform boosted clique on two input circles
-#' 
+#'
 #' @name boosted_clique
 #' @param circle_in The input circle
 #' @param circle_ref The reference circle
@@ -196,33 +196,33 @@ boosted_clique <- function(circle_in, circle_ref, ncross_in_bins = 30, xbins_in 
     c_in <- smart_sample(circle_in,sample_bins=min(ncross_in_bins,nrow(circle_in)),xbins=xbins_in,sample_size=ncross_in_bin_size,seed=seed)
     ## Smart sample reference circle points or full points
     c_ref <- smart_sample(circle_ref,sample_bins=min(ncross_ref_bins,nrow(circle_ref)),xbins=xbins_ref,sample_size=ncross_ref_bin_size,seed=seed)
-    
+
     # set.seed(seed)
     # c_in <- as.matrix(circle_in[sample(1:nrow(circle_in),ncross_in_bins),])
     # c_ref <- as.matrix(circle_ref[sample(1:nrow(circle_ref),ifelse(is.null(ncross_ref_bins),nrow(circle_ref),ncross_ref_bins)),])
-    
+
     ## Data dimesion for later reference within function
     la <- nrow(c_in)					## Number of minutiae in image 1
     lb <- nrow(c_ref)					## Number of minutiae in image 2
     lg <- la * lb						## Number of vertices in the "product graph"
     ##############################################################################################################
-    
+
     ##############################################################################################################
     ## Prepare distance matrix for adjancey lists calculations
     ##############################################################################################################
     ## Pairwise distance matrix between vertices in input and reference data
     dist_in <- as.matrix(dist(c_in))
     dist_ref <- as.matrix(dist(c_ref))
-    
+
     ## Fill NA's to avoid loops
     dist_in[row(dist_in)==col(dist_in)] <- NA
     dist_ref[row(dist_ref)==col(dist_ref)] <- NA
-    
+
     ## Split matrix to list of columns so as to vectorize calculation of adjacency list
     distl_in <- as.list(as.data.frame(dist_in))
     distl_ref <- as.list(as.data.frame(dist_ref))
     dist_l_grid <- expand.grid(distl_ref,distl_in)
-    
+
     ## Adding k1 verice 1 name to list so as to get edge match for k2<k1 only and thus avoiding values
     ## in upper triangulation matrix (Visualising edge match matrix lg*lg)
     dist_l_grid$node <- 1:nrow(dist_l_grid)
@@ -231,21 +231,21 @@ boosted_clique <- function(circle_in, circle_ref, ncross_in_bins = 30, xbins_in 
     rownames(dist_l_grid) <- 1:nrow(dist_l_grid)
     gc()
     ##############################################################################################################
-    end_time_i <- Sys.time()		
+    end_time_i <- Sys.time()
     if(verbose) cat(paste("Prepared circles data for edge matching. Took",round(difftime(end_time_i,start_time_i,units="secs")),"Seconds.\n\n\n"))
-    
-    
-    
-    
-    
+
+
+
+
+
     start_time_al <- Sys.time()
     if(verbose) cat("Calculating adjacency list.\n")
     ##############################################################################################################
-    ## Adjacency list prepasration (Vectorized Code with parallel processing [*nix style]) 
+    ## Adjacency list prepasration (Vectorized Code with parallel processing [*nix style])
     ##############################################################################################################
     ## Split data to distribute work to cores
     dist_l_grid_core_assign <- split(dist_l_grid,floor(seq(0,((num_cores*4)-.01),length.out=nrow(dist_l_grid))))
-    
+
     ## Windows
     if (!is.null(cl)) {
         final_ks <- do.call(c,parLapply(cl, dist_l_grid_core_assign,function(x,eps,la,lb) {gc();apply(x,1,get_edge_vertice,eps=eps,la=la,lb=lb)},eps=eps,la=la,lb=lb))
@@ -253,12 +253,12 @@ boosted_clique <- function(circle_in, circle_ref, ncross_in_bins = 30, xbins_in 
     } else {
         final_ks <- do.call(c,mclapply(dist_l_grid_core_assign,function(x,eps,la,lb) {gc();apply(x,1,get_edge_vertice,eps=eps,la=la,lb=lb)},eps=eps,la=la,lb=lb,mc.cores=num_cores))
     }
-    
+
     gc()
     ##############################################################################################################
     end_time_al <- Sys.time()
     if(verbose) cat(paste("Calculated adjacency list. Took",round(difftime(end_time_al,start_time_al,units="secs")),"Seconds.\n\n\n"))
-    
+
     start_time_mc <- Sys.time()
     if(verbose) cat("Finding largest clique.\n")
     ##############################################################################################################
@@ -278,13 +278,13 @@ boosted_clique <- function(circle_in, circle_ref, ncross_in_bins = 30, xbins_in 
     ext <- ifelse(get_os() == "win64", ".exe", "")
     clique_max <- system(paste0(system.file(package = "shoeprintr", "bin", get_os(), paste0("pmc", ext)), " -f ", file.path(mytempdir, "edge.mtx"), " -a 0"), intern = TRUE)
     clique_max <- as.numeric(unlist(strsplit(gsub("Maximum clique: ","",clique_max[grepl("^Maximum clique: ",clique_max)])," ")))
-    
+
     ## Cleanup
     gc()
     ##############################################################################################################
     end_time_mc <- Sys.time()
     if(verbose) cat(paste("Calculated largest clique. Took",round(difftime(end_time_mc,start_time_mc,units="secs")),"Seconds.\n\n\n"))
-    
+
     ##############################################################################################################
     ## Get and return clique stats
     ##############################################################################################################
@@ -293,7 +293,7 @@ boosted_clique <- function(circle_in, circle_ref, ncross_in_bins = 30, xbins_in 
 }
 
 #' Adjust it to (0,0) in the left lower corner
-#' 
+#'
 #' @name leftcorner_cent
 #' @param data The input data
 #' @export
@@ -326,7 +326,7 @@ get_circle_fix <- function(x,y,rad_pct,data)
 }
 
 ##############################################################################################################
-## Generate circle on given x,y and radius 
+## Generate circle on given x,y and radius
 ##############################################################################################################
 get_circle_rad_fix <- function(x,y,cir_rad,data)
 {
@@ -335,9 +335,9 @@ get_circle_rad_fix <- function(x,y,cir_rad,data)
 }
 
 #' @title Matches an input and a reference shoeprint
-#' 
+#'
 #' @description Function to perform matching of footprints by matching all the candidate circles with input circles. This function performs initial matching between 3 circles on the input print and 27 candidate circles on the reference shoeprint. After the initial cliques, it select 2 best circles for each input circle and performs reinforcement matching on them. In reinforcement matching, full points on reference circle are considered instead of user defined. The function returns the final circle parameters and the statistics of the triangle formed.
-#' 
+#'
 #' @name match_print
 #' @param print_in The input print
 #' @param print_ref The reference print
@@ -359,11 +359,11 @@ get_circle_rad_fix <- function(x,y,cir_rad,data)
 #' @import grDevices
 #' @importFrom stats dist
 #' @export
-#' @examples 
+#' @examples
 #' \dontrun{
 #' data(input_example)
 #' data(reference_example)
-#' 
+#'
 #' ## Transform all the points to have (0,0) at lower left corner.
 #' print_in <- leftcorner_cent(reference_example)
 #' print_ref <- leftcorner_cent(reference_example)
@@ -372,7 +372,7 @@ get_circle_rad_fix <- function(x,y,cir_rad,data)
 #' print_stats <- match_print(print_in, print_ref,
 #'                           ncross_in_bins = 30, xbins_in = 20, ncross_in_bin_size = 1,
 #'                           ncross_ref_bins = NULL, xbins_ref = 30, ncross_ref_bin_size = NULL,
-#'                           eps = .75, seed = 1, num_cores = parallel::detectCores(), 
+#'                           eps = .75, seed = 1, num_cores = parallel::detectCores(),
 #'                           plot = TRUE, verbose = FALSE)
 #' print_stats
 #' }
@@ -384,17 +384,17 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     ## Generate 1 circles on input shoeprint
     if (is.null(circles_input)) {
         circles_dims <- apply(print_in,2,function(x) max(x)-min(x))
-        circle_centers <- matrix(c(	.25,.8,			## 1/4 on x, 4/5 on y 
-                                    .25,.3,			## 1/4 on x, 1/5 on y 
-                                    .7,.6			## 3/4 on x, 1/2 on y 
+        circle_centers <- matrix(c(	.25,.8,			## 1/4 on x, 4/5 on y
+                                    .25,.3,			## 1/4 on x, 1/5 on y
+                                    .7,.6			## 3/4 on x, 1/2 on y
         ),3,byrow=TRUE,dimnames=list(NULL,c("x","y"))) %*% diag(circles_dims)
         circle_centers <- cbind(circle_centers,c(.4,.4,.4))
     } else {
         circle_centers <- circles_input
     }
-    
+
     circles_in <- apply(circle_centers,1,function(x,data) get_circle_fix(x[1],x[2],x[3],data),data=print_in)
-    
+
     ## Generate candidate circles on reference shoeprint
     if (is.null(circles_reference)) {
         rad_pct <- .5
@@ -408,26 +408,26 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     }
     ##############################################################################################################
     ##############################################################################################################
-    
-    
+
+
     ##############################################################################################################
     ## Perform initial matching on circles
     ## 3 on Input circle and 27 on Reference circle
     ##############################################################################################################
     match_grid <- expand.grid(circles_ref,circles_in)
-    
+
     ## Use a cluster for parallel computation if running on windows
     cl <- NULL
     if (get_os() == "win64") cl <- makeCluster(num_cores, renice = 0)
-    
+
     match_result <- lapply(1:nrow(match_grid), function(match_idx) {
         cat(paste("Matching circle pair",match_idx,"out of", nrow(match_grid), "\n"))
-        
+
         circle_in <- match_grid[match_idx,2][[1]]
         circle_ref <- match_grid[match_idx,1][[1]]
-        
+
         if (nrow(circle_ref) > 0) {
-            boosted_clique(circle_in, circle_ref, 
+            boosted_clique(circle_in, circle_ref,
                            ncross_in_bins=ncross_in_bins,xbins_in=xbins_in,ncross_in_bin_size=ncross_in_bin_size,
                            ncross_ref_bins=ncross_ref_bins,xbins_ref=xbins_ref,ncross_ref_bin_size=ncross_ref_bin_size,
                            eps=eps,seed=seed,num_cores=num_cores,plot=plot,verbose=verbose,cl=cl
@@ -438,8 +438,8 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     })
     ##############################################################################################################
     ##############################################################################################################
-    
-    
+
+
     ##############################################################################################################
     ## Select best 2 circles against each of input circle for reinforcement learning
     ## Best criterion is whichever have largest input circle overlap
@@ -452,9 +452,9 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     circles_in_reinf <- rep(circles_in,each=2)
     ##############################################################################################################
     ##############################################################################################################
-    
+
     ##############################################################################################################
-    ## Perform reinforcement learning 
+    ## Perform reinforcement learning
     ##############################################################################################################
     match_result_reinf <- lapply(1:length(circles_in_reinf), function(match_idx_reinf) {
         cat(paste("Reinforcement matching circle pair",match_idx_reinf,"out of 6...\n"))
@@ -464,7 +464,7 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
         )
     })
     if (!is.null(cl)) stopCluster(cl)
-    
+
     ##############################################################################################################
     ## Prepare plot and  return output
     ##############################################################################################################
@@ -474,15 +474,15 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     circles_match <- match_result_reinf[unlist(best_match),]
     best_match_params <- match_result_reinf[unlist(best_match),c("new_center_x","new_center_y","new_radius")]
     circles_ref_out <- apply(best_match_params,1,function(x,data) get_circle_rad_fix(x[1],x[2],x[3],data),data=print_ref)
-    
-    
+
+
     circles_match <- cbind(circle_centers[,1:2],circles_match[,c("new_center_x","new_center_y","new_radius","rotation_angle","input_overlap")])
     names(circles_match) <- c("Fixed_circle_X","Fixed_circle_Y","Match_circle_X","Match_circle_Y","Match_circle_Radius","Rotation_angle","Input_circle_overlap_pct")
-    
+
     ## Congurent Triangle output
     Fixed_triangle <- as.matrix(dist(circles_match[,c("Fixed_circle_X","Fixed_circle_Y")]))
     Match_Triangle <- as.matrix(dist(circles_match[,c("Match_circle_X","Match_circle_Y")]))
-    
+
     ## Plot
     dev.off()
     par(mfrow=c(1,2))
@@ -494,8 +494,9 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     points(circles_ref_out[[1]],col="red")
     points(circles_ref_out[[2]],col="yellow")
     points(circles_ref_out[[3]],col="green")
-    
-    
+
+    # Testing a change
+
     ## Return Stats
     return(cbind(	circles_match,
                   data.frame(	Triangle_sides = c("A-B","A-C","B-C"),
@@ -503,5 +504,5 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
                               Match_circle_side_length = Match_Triangle[row(Match_Triangle)<col(Match_Triangle)]
                   )
     ))
-    
+
 }
