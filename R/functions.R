162283f3f66ac1dcc66c002e8f3f1902541c42dd
Eric Hare <ericrhare@gmail.com>
2017-09-13 15:41:39 -0500

    Print the plot

--- R/functions.R
+++ R/functions.R
@@ -535,7 +535,7 @@ match_print <- function(print_in, print_ref, circles_input = NULL, circles_refer
     #points(circles_ref_out[[2]],col="yellow")
     #points(circles_ref_out[[3]],col="green")
     
-    try(grid.arrange(p1, p2, ncol = 2), silent = TRUE)
+    try(print(grid.arrange(p1, p2, ncol = 2)), silent = TRUE)
 
     ## Return Stats
     return(cbind(	circles_match,
